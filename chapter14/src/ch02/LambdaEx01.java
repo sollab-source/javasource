8b9506fbbbcc15a58a12a557e07443b46399a781
kyung <kyung5896@gmail.com>
2024-10-08 00:34:02 +0900

    코드 정리

--- chapter14/src/ch02/LambdaEx01.java
+++ chapter14/src/ch02/LambdaEx01.java
@@ -5,9 +5,9 @@ interface MyFunction1 {
 	int max(int a, int b);
 }
 
-public class LambdaEx1 {
+public class LambdaEx01 {
 	public static void main(String[] args) {
-		
+//		테스트
 		MyFunction1 f = (a, b) -> a > b ? a : b;
 		System.out.println("둘 중 큰 숫자는 ? "+f.max(12, 19));
 	}
--- chapter14/src/ch02/LambdaEx02.java
+++ chapter14/src/ch02/LambdaEx02.java
@@ -5,9 +5,8 @@ interface MyFunction2 {
 	int square(int x);
 }
 
-public class LambdaEx2 {
-	public static void main(String[] args) {
-		
+public class LambdaEx02 {		
+	public static void main(String[] args) {		
 		MyFunction2 f = (x) -> x * x;
 		System.out.println("제곱 ? "+f.square(12));
 	}
