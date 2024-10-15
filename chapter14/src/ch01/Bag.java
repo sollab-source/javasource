f89e4e611429bfacf1015416192278eb93fe0f97
kyung <kyung5896@gmail.com>
2024-10-15 18:42:46 +0900

    inner 변경 내용

--- chapter14/src/ch01/Bag.java
+++ chapter14/src/ch01/Bag.java
@@ -1,29 +1,37 @@
 package ch01;
 
 public class Bag {
-	 
-    private String name;
-    private Long price;
- 
- 
-    public Bag(String name, Long price) {
-        this.name = name;
-        this.price = price;
-    }
- 
-    class Pouch {
-        private String pouchName;
-        private String color;
- 
-        static int k = 5; // static 멤버 사용 불가
-        static final int cost = 5000; // 하지만 상수인 final static은 사용 가능
- 
-        public void printMember() {
-            System.out.println("name = " + name); // 외부 클래스의 멤버에 접근 가능 ( private 여도 같은 클래스 내에서 접근하므로 ok )
-            System.out.println("price = " + price); // 외부 클래스의 멤버에 접근 가능 ( private 여도 같은 클래스 내에서 접근하므로 ok )
- 
-            System.out.println("pouchName = " + pouchName); // 내부 클래스의 멤버
-            System.out.println("color = " + color); // 내부 클래스의 멤버
-        }
-    }
+
+	private String name;
+	private Long price;
+
+	public Bag(String name, Long price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	class Pouch {
+		private String pouchName;
+		private String color;
+
+		//https://docs.oracle.com/javase/specs/jls/se16/preview/specs/local-statics-jls.html
+		//https://bugs.openjdk.org/browse/JDK-8254321
+		// static 멤버 사용 불가
+		static int k = 5;
+		// 하지만 상수인 final static은 사용 가능
+		static final int cost = 5000;
+
+		public void printMember() {
+			// 외부 클래스의 멤버에 접근 가능 ( private 여도 같은 클래스 내에서 접근하므로 ok )
+			System.out.println("name = " + name);
+			// 외부 클래스의 멤버에 접근 가능 ( private 여도 같은 클래스 내에서 접근하므로 ok )
+			System.out.println("price = " + price);
+
+			// 내부 클래스의 멤버 사용
+			System.out.println("pouchName = " + pouchName); 
+			System.out.println("color = " + color); 
+		}
+		
+		public static void test() {}
+	}
 }
--- chapter14/src/ch01/BagEx.java
+++ chapter14/src/ch01/BagEx.java
@@ -5,10 +5,13 @@ import ch01.Bag.Pouch;
 public class BagEx {
 
 	public static void main(String[] args) {
-		Bag bag = new Bag("test",100000L);
+		Bag bag = new Bag("chanel",100000L);
 		
-		Bag.Pouch p = bag.new Pouch();
-		p.printMember();
+		//inner 클래스의 경우에는 직접 객체 생성 불가
+		//Pouch pouch = new Pouch();
+		
+		//Bag.Pouch p = bag.new Pouch();
+		//p.printMember();
 		
 		
 		System.out.println(Pouch.k);
