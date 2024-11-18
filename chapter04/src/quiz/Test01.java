0144d83a3362893faab248b76a8722c62d58f387
seungchan0324 <winchan324@gmail.com>
2024-11-18 12:04:10 +0900

    chapter04 quiz 수정 및 한글깨짐 test

--- chapter04/src/quiz/Test01.java
+++ chapter04/src/quiz/Test01.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Test01 {
 
 	public static void main(String[] args) {
-		
 		Scanner scanner = new Scanner(System.in);
+
 		System.out.print("숫자를 입력하세요: ");
-		
-		int n = scanner.nextInt();
-		
-		if(n % 2 == 0) {
-			System.out.println(n + "는 짝수입니다.");
-		}else {
-			System.out.println(n + "는 홀수입니다.");			
+		int number = scanner.nextInt();
+
+		if (number % 2 == 0) {
+			System.out.println(number + "는 짝수입니다.");
+		} else {
+			System.out.println(number + "는 홀수입니다.");
 		}
+		scanner.close();
 	}
 }
--- chapter04/src/quiz/Test02.java
+++ chapter04/src/quiz/Test02.java
@@ -3,16 +3,15 @@ package quiz;
 import java.util.Scanner;
 
 public class Test02 {
-	
+
 	public static void main(String[] args) {
-	
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("구구단 몇 단을 출력할까요? ");
+		int dan = scanner.nextInt();
 
-		int n = scanner.nextInt();
-		
-		for(int i = 1; i < 10; i++) {
-			System.out.println(n + " * " + i + " = " + n * i);
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(dan + " * " + i + " = " + (dan * i));
 		}
+		scanner.close();
 	}
 }
