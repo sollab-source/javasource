ae155a07ed6e5845ea75bca963cc8124e4fcb35c
seungchan0324 <winchan324@gmail.com>
2025-01-30 19:20:41 +0900

    Extends 수정

--- chapter07/src/ch01/inheritanceEx02/Subway.java
+++ chapter07/src/ch01/inheritanceEx02/Subway.java
@@ -3,11 +3,5 @@ package ch01.inheritanceEx02;
 public class Subway extends PublicTransport {
 
 	int subwayLine;// 지하철 호선
-	int charge;// 요금
-	int passengerCount;// 승객 수
-
-	// 매출액 계산
-	public int calculateSales() {
-		return charge * passengerCount;
-	}
+	// PublicTransport 코드
 }
