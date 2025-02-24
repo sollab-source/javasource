1aef8692e69fb244a6cd8a45516847e6959ae532
soldesk_lab <pjky5@studydesk.co.kr>
2025-02-24 21:36:14 +0900

    Delete chapter12/src directory

--- chapter12/src/ch04/comparatoremployeeEx08/Employee.java
+++
@@ -1,9 +0,0 @@
-package ch04.comparatoremployeeEx08;
-
-public record Employee(String name, int age) {
-
-	@Override
-	public String toString() {
-		return name + ": " + age + "세";
-	}
-}
--- chapter12/src/ch05/memberhashmapEx01/Member.java
+++
@@ -1,9 +0,0 @@
-package ch05.memberhashmapEx01;
-
-public record Member(int memberId, String memberName) {
-
-	@Override
-	public String toString() {
-		return "Member [memberId=" + memberId + ", memberName=" + memberName + "]";
-	}
-}
