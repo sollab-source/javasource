9786f2eb3f5dc9a980bb02493b9d9ef76e645dfb
soldesk_lab <pjky5@studydesk.co.kr>
2025-02-24 21:36:06 +0900

    Delete chapter11/src directory

--- chapter11/src/ch04/MinusException.java
+++
@@ -1,9 +0,0 @@
-package ch04;
-
-@SuppressWarnings("serial")
-public class MinusException extends Exception {
-	
-	public MinusException(int point) {
-		super("입력한 포인트는 음수입니다. : "+ point);
-	}
-}
--- chapter11/src/ch04/UpException.java
+++
@@ -1,8 +0,0 @@
-package ch04;
-
-@SuppressWarnings("serial")
-public class UpException extends Exception {
-	public UpException(int point) {
-		super("입력한 포인트가 50보다 큽니다. : "+point);
-	}
-}
